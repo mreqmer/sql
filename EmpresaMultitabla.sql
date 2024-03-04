d5ed7c16addb352684c37ee5bdd2111c6734b6f4
mreqmer <marta.requejo@iesnervion.es>
2024-03-04 09:15:17 +0100

    primerito

---
+++ EmpresaMultitabla.sql
@@ -0,0 +1,65 @@
+USE Empresa
+
+GO
+
+--1. Añadir una nueva oficina para la ciudad de Madrid, con el número de oficina 30, con un objetivo
+--de 10.000 euros y región ‘Centro’. Suponer que conocemos el orden de los campos en la tabla.
+
+
+--2. Igual que el ejercicio anterior suponiendo que no sabemos cual es el orden de los campos en la
+
+
+--3. Insertar tus datos como nuevo empleado. Utilizar como numemp los tres últimos dígitos del dni, la
+--oficina 23, el puesto “Programador”, sin jefe, con una cuota de 1000 y las ventas a 0. En el contratola fecha de hoy.
+
+
+--4. Insertar un nuevo cliente con tu nombre y utilizar como numclie 999. Dejar el resto de campos a
+--su valor por defecto.
+
+
+--5. Insertar los empleados que no tienen jefes como clientes. Como número de clientes utilizaremos
+--el mismo número de empleado, ellos mismo serán sus propios responsable y el límite de crédito será 0.
+
+
+--6. De la base de datos Robótica, insertar los dependientes como clientes de la base de datos Empresa. 
+--Para generar el numclie utilizaremos la suma del año, mes y día de la fecha de nacimiento de los
+--dependientes. El resp será el 108 y el límite de crédito 1000 euros.
+
+
+--7. Subir un 5% el precio de todos los productos del fabricante ‘ACI’.
+
+
+--8. Incrementar en uno la edad de los empleados.
+
+
+--9. Cambiar los empleados de la oficina 21 a la oficina 30.
+
+
+--10. De los empleados que trabajan en Cádiz, disminuir su cuota en un 10%.
+
+
+--11. Bajar 100 euros el precio de los productos de los que no se han realizado ningún pedido. Hay que tener cuidado que no queden precios negativos.
+
+
+--12. Modificar el nombre de los empleados para eliminar el segundo nombre o apellido (apellidos) de su nombre.
+
+
+--13. Cambiar la cuota de todos los empleados a 1000 euros.
+
+
+--14. Eliminar los pedidos cuyo responsable es el empleado 105.
+
+
+--15. Eliminar los tres clientes con menor límite de crédito.
+
+
+--16. En un ejercicio anterior hemos insertado un nuevo empleado con nuestros datos. Eliminar dicho registro.
+
+
+--17. Eliminar las oficinas que no tengan empleados.
+
+
+--18. Elimiar cualquier rastro del cliente 2103 (datos y pedidos).
+
+
+--19. Eliminar los empleado que han realizado al menos un pedido del fabricante ‘ACI’.
